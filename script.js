6964c3049070ae3e12f0819ea1c6ad45c0e769b8
Tom Bergqvist <bergqvist.tom@gmail.com>
2019-03-25 03:42:05 +0100

    Cleaned

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 import * as Web from './WebModule.js'
 
-document.getElementById("convertBtn").addEventListener("click", function(){
+document.getElementById("convertBtn").addEventListener("click", function () {
     let base = document.getElementById("base").value
     let target = document.getElementById("target").value
     let amount = document.getElementById("amount").value
 
-    let result = Web.exchange(base, target, amount)
-    result.then(setResult)
+    Web.exchange(base, target, amount).then(setResult)
 })
 
-function setResult(result, target){
+function setResult(result) {
     document.getElementById("result").innerHTML = result
 }
